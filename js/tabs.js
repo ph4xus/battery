074539e3027917186b54497cc0de9a23605de944
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-03-21 16:44:25 -0600

    k so more changes

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,9 @@
 const all = document.querySelectorAll('section');
 const HomePage = Array.from(all).slice(0, 4);
-
 const allgames = document.querySelectorAll('section')[4];
+const allGamesLi = document.getElementById('all-games');
+const mainLi = document.getElementById('home');
+
 
 function hide_main() {
     HomePage.forEach(section => {
@@ -22,3 +24,14 @@ function display_main() {
 function display_allgames() {
   allgames.style.display = 'block';
 }
+
+allGamesLi.addEventListener('click', function() {
+  hide_main();
+  display_allgames();
+});
+
+mainLi.addEventListener('click', function() {
+  hide_allgames();
+  display_main();
+});
+
