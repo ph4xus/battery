befecb5f7aff2e3b2f8f33686d1b7243a8569549
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-03-21 16:29:24 -0600

    everything

--- js/tabs.js
+++ js/tabs.js
@@ -3,9 +3,22 @@ const HomePage = Array.from(all).slice(0, 4);
 
 const allgames = document.querySelectorAll('section')[4];
 
-function hide() {
+function hide_main() {
     HomePage.forEach(section => {
         section.style.display = 'none';
       });
 }
 
+function hide_allgames() {
+  allgames.style.display = 'none';
+}
+
+function display_main() {
+  HomePage.forEach(section => {
+      section.style.display = 'block';
+    });
+}
+
+function display_allgames() {
+  allgames.style.display = 'block';
+}
